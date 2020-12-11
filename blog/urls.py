85ab4fd06393ddcf538eaaa595fa22ef693d9e41
saeed <saeed@pop-os.localdomain>
2020-12-12 00:49:50 +0330

    successfully migrated blog model

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path, include
-from .views import home, single, categories, login_view, logout_view
+from .views import home, single, categories
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('posts/', home, name='posts_archive'),
     path('posts/<slug:slug>', single, name='post_single'),
-    path('cats/<slug:slug>', categories, name='cat_archives'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout')
+    path('cats/<slug:slug>', categories, name='cat_archives')
 ]
